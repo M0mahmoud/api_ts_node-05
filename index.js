a6ba56e868491c9d6d008d37582e31dd0ecf2ef7
M0mahmoud <houdmohamed85@gmail.com>
2024-02-23 19:48:25 +0200

    Adding Call Points

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import express from "express";
 import { rateLimit } from "express-rate-limit";
 import TelegramBot from "node-telegram-bot-api";
+import Call from "./Call.Model.js";
 import RecentSearch from "./RecentSearch.Model.js";
 import User from "./User.Model.js";
 import { getFacebookId, isValidFacebookProfileLink } from "./helpers.js";
@@ -69,6 +70,54 @@ Login Within In Our App`,
   }
 });
 
+bot.onText(/\/usercall/, async (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    await UserDB();
+    const isAdmin = await User.findOne({ id: chatId });
+    if (!isAdmin) {
+      bot.sendMessage(chatId, "User not found");
+      return;
+    }
+    if (
+      isAdmin.id !== process.env.ADMIN_05 &&
+      isAdmin.id !== process.env.ADMIN_USF
+    ) {
+      bot.sendMessage(chatId, "Forbidden, Only Admins...");
+      return;
+    }
+
+    await bot.sendMessage(
+      chatId,
+      "Plz,Send Code Of The User For Adding More Call Points...",
+      {
+        allow_sending_without_reply: false,
+      }
+    );
+
+    bot.once("message", async (msg) => {
+      const code = msg.text;
+      const user = await Call.findOne({ code });
+      if (!user) {
+        return bot.sendMessage(chatId, "User not found!");
+      }
+      bot.sendMessage(chatId, "Enter Number Of Points!");
+
+      bot.once("message", async (msg) => {
+        const points = msg.text;
+        user.points = Number(points);
+        await user.save();
+        return bot.sendMessage(
+          chatId,
+          `Done, ${user.code} has ${user.points} now`
+        );
+      });
+    });
+  } catch (error) {
+    console.log("Error adding points:", error);
+    bot.sendMessage(chatId, "Sorry, an error occurred while adding points!");
+  }
+});
 bot.onText(/\/addpoints/, async (msg) => {
   const chatId = msg.chat.id;
   try {
