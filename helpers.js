4263c3242a3bcbed249d7c63705f401ea85546a4
M0mahmoud <houdmohamed85@gmail.com>
2024-06-30 03:47:46 +0300

    Fix facebookid Command

--- helpers.js
+++ helpers.js
@@ -1,24 +1,22 @@
+import cheerio from "cheerio";
+import fetch from "node-fetch";
+
 export function isValidFacebookProfileLink(link) {
   // Basic check for Facebook profile link format
-  const facebookRegex =
-    /^https?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9._]+(\?[^#\s]*)?$/;
+  const facebookRegex = /^https?:\/\/(www\.)?facebook\.com\/[a-zA-Z0-9._]+(\?[^#\s]*)?$/;
   return facebookRegex.test(link);
 }
 export async function getFacebookId(link) {
-  const res = await fetch(
-    "https://aloneperson.pythonanywhere.com/get_facebook_id",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: link,
-        api_key: "IDFINDER_FJg1NtDfqNrrTEX4zznDEJDaBJU9hR",
-      }),
-    }
-  );
-  const data = await res.json();
+  const response = await fetch(link);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  const profileIdElement = $("meta[property='al:android:url']").attr("content");
 
-  return data.facebook_id;
+  if (profileIdElement) {
+    const profileId = profileIdElement.split("/").pop();
+    return profileId;
+  } else {
+    console.log("Can't Get ID");
+    return null;
+  }
 }
